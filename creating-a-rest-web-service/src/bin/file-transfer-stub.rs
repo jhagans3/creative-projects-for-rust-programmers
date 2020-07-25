d7a9249a5a4dc0d6641600ef4870e9a6f79de9c0
James Hagans <jhagans@acm.org>
2020-07-25 14:53:58 -0400

    actix 2 hello world

--- creating-a-rest-web-service/src/bin/file-transfer-stub.rs
+++ creating-a-rest-web-service/src/bin/file-transfer-stub.rs
@@ -16,99 +16,21 @@
 // Uploading file "data_*.txt" ... Uploaded file "data_17.txt"
 // Invalid URI: "/a/b"
 
-use actix_web::{web, web::Path, App, HttpRequest, HttpResponse, HttpServer, Responder};
-use std::io::Write;
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 
-fn flush_stdout() {
-    std::io::stdout().flush().unwrap();
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
 }
-
-fn delete_file(info: Path<(String,)>) -> impl Responder {
-    let filename = &info.0;
-    print!("Deleting file \"{}\" ... ", filename);
-    flush_stdout();
-
-    // TODO: Delete the file.
-
-    println!("Deleted file \"{}\"", filename);
-    HttpResponse::Ok()
-}
-
-// fn download_file(info: Path<(String,)>) -> impl Responder {
-//     let filename = &info.0;
-//     print!("Downloading file \"{}\" ... ", filename);
-//     flush_stdout();
-
-//     // TODO: Read the contents of the file.
-//     let contents = "Contents of the file.\n".to_string();
-
-//     println!("Downloaded file \"{}\"", filename);
-//     HttpResponse::Ok().content_type("text/plain").body(contents)
-// }
-
-// fn upload_specified_file(info: Path<(String,)>) -> impl Responder {
-//     let filename = &info.0;
-//     print!("Uploading file \"{}\" ... ", filename);
-//     flush_stdout();
-
-//     // TODO: Get from the client the contents to write into the file.
-//     let _contents = "Contents of the file.\n".to_string();
-
-//     // TODO: Create the file and write the contents into it.
-
-//     println!("Uploaded file \"{}\"", filename);
-//     HttpResponse::Ok()
-// }
-
-// fn upload_new_file(info: Path<(String,)>) -> impl Responder {
-//     let filename = &info.0;
-//     print!("Uploading file \"{}*.txt\" ... ", filename);
-//     flush_stdout();
-
-//     // TODO: Get from the client the contents to write into the file.
-//     let _contents = "Contents of the file.\n".to_string();
-
-//     // TODO: Generate new filename and create that file.
-//     let file_id = 17;
-
-//     let filename = format!("{}{}.txt", filename, file_id);
-
-//     // TODO: Write the contents into the file.
-
-//     println!("Uploaded file \"{}\"", filename);
-//     HttpResponse::Ok().content_type("text/plain").body(filename)
-// }
-
-// fn invalid_resource(req: HttpRequest) -> impl Responder {
-//     println!("Invalid URI: \"{}\"", req.uri());
-//     HttpResponse::NotFound()
-// }
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    let server_address = "127.0.0.1:8080";
-    println!("Listening at address {} ...", server_address);
     HttpServer::new(|| {
         App::new()
-        .route("/{filename:.*}", web::get().to(index))
-            // .service(
-                // web::resource("/{filename}")
-                    .route(web::delete().to(delete_file))
-                //     .route(web::get().to(download_file))
-                //     .route(web::put().to(upload_specified_file))
-                //     .route(web::post().to(upload_new_file)),
-            // )
-            // .default_service(web::route().to(invalid_resource))
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
     })
-    .bind(server_address)?
+    .bind("127.0.0.1:8000")?
     .run()
+    .await
 }
-
-    // HttpServer::new(|| {
-    //     App::new()
-    //         .route("/", web::get().to(index))
-    //         .route("/again", web::get().to(index2))
-    // })
-    // .bind("127.0.0.1:8088")?
-    // .run()
-    // .await
